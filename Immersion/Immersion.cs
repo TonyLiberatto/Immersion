04b53d8da94923c1d1134c1fd639fdd952401ca9
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-12-07 16:29:49 -0500

    include setmaterial command

--- Immersion/Immersion.cs
+++ Immersion/Immersion.cs
@@ -7,6 +7,7 @@ using System;
 using System.IO;
 using Vintagestory.API.Server;
 using System.Collections.Generic;
+using Vintagestory.API.MathTools;
 
 [assembly: ModInfo("Immersion",
     Description  = "This mod Requires New World Creation. Adds more Animals, Plants, blocks and tools",
@@ -25,6 +26,28 @@ namespace Immersion
         public override void StartServerSide(ICoreServerAPI Api)
         {
             sapi = Api;
+            sapi.RegisterCommand("setmaterial", "Set the material of currently looked at chiseled block", "[block code]", (player, groupId, args) =>
+            {
+                BlockPos pos = player.CurrentBlockSelection.Position;
+                BlockEntityChisel bet = sapi.World.BlockAccessor.GetBlockEntity(pos) as BlockEntityChisel;
+                Block block = sapi.World.GetBlock(new AssetLocation(args.PopWord()));
+
+                if (bet == null)
+                {
+                    player.SendMessage(groupId, "Not looking at a chiseled block. Must look at one to set its material", EnumChatType.CommandError);
+                    return;
+                }
+                if (block == null)
+                {
+                    player.SendMessage(groupId, "Did not supply a valid block code", EnumChatType.CommandError);
+                    return;
+                }
+
+                bet.MaterialIds = new int[] { block.Id };
+                bet.MarkDirty(true);
+                player.SendMessage(groupId, "Material set.", EnumChatType.CommandError);
+            }
+            , Privilege.controlserver);
         }
 
         public override void StartClientSide(ICoreClientAPI Api)
