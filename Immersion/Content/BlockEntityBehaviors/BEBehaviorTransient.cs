1049da1ff90aaa519a7017b432f1fa09dea17116
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-12-13 20:42:09 -0500

    add RequiredSunlight transition condition to BEBehaviorTransient

--- Immersion/Content/BlockEntityBehaviors/BEBehaviorTransient.cs
+++ Immersion/Content/BlockEntityBehaviors/BEBehaviorTransient.cs
@@ -10,6 +10,15 @@ using Vintagestory.GameContent;
 
 namespace Immersion
 {
+    public class TransitionConditions
+    {
+        public TransitionConditions(int RequiredSunlight)
+        {
+            this.RequiredSunlight = RequiredSunlight;
+        }
+        public int RequiredSunlight { get; set; } = -1;
+    }
+
     public class BEBehaviorTransient : BlockEntityBehavior
     {
         double transitionAtTotalDays = -1;
@@ -17,6 +26,7 @@ namespace Immersion
         string fromCode;
         string toCode;
         public Block OwnBlock { get => Blockentity.Block; }
+        public TransitionConditions conditions;
 
         public BEBehaviorTransient(BlockEntity blockentity) : base(blockentity)
         {
@@ -32,6 +42,7 @@ namespace Immersion
             }
             fromCode = properties["convertFrom"].AsString()?.WithDomain(OwnBlock.Code.Domain);
             toCode = properties["convertTo"].AsString()?.WithDomain(OwnBlock.Code.Domain);
+            conditions = properties["transitionConditions"]?.AsObject<TransitionConditions>(null);
 
             if (fromCode != null && toCode != null)
             {
@@ -46,6 +57,7 @@ namespace Immersion
         public void CheckTransition(float dt)
         {
             if (transitionAtTotalDays > Api.World.Calendar.TotalDays) return;
+            if (Api.World.BlockAccessor.GetLightLevel(this.Blockentity.Pos, EnumLightLevelType.MaxTimeOfDayLight) < (conditions?.RequiredSunlight ?? -1)) return;
 
             Block block = Api.World.BlockAccessor.GetBlock(Pos);
             Block tblock;
