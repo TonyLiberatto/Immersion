8542748d163152cfa2d87335a1bb229100bb45e3
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2020-03-29 16:06:25 -0400

    work on rivers more, fix noise repetitition in deep ore bits

--- Source/Systems/WorldGen/GenRivers.cs
+++ Source/Systems/WorldGen/GenRivers.cs
@@ -19,9 +19,11 @@ namespace Immersion
         int noiseSizeRiver;
         public ImmersionGlobalConfig config { get => api.ModLoader.GetModSystem<ModifyLakes>().config; }
         NormalizedSimplexNoise noise;
+        LakeBedLayerProperties lakebedLayerConfig;
+        ImmersionGlobalConfig immersionConfig;
 
         public int chunksize2 { get => chunksize > 0 ? chunksize : 32; }
-        public override double ExecuteOrder() => 0.1;
+        public override double ExecuteOrder() => 0.45;
 
         public override bool ShouldLoad(EnumAppSide forSide) => false;// forSide == EnumAppSide.Server;
 
@@ -48,8 +50,12 @@ namespace Immersion
             LoadGlobalConfig(api);
             long seed = api.WorldManager.Seed;
             riverGen = new MapLayerPerlin(seed + 46841, 6, 0.1f, 16, 255, new double[] { 0.02f, 0.02f, 0.02f, 0.02f, 0.02f, 0.02f });
-            noiseSizeRiver = api.WorldManager.RegionSize / 16;
+            riverGen = new MapLayerPerlinWobble(seed + 16789, riverGen, 6, 0.1f, 4, 1);
+
+            noiseSizeRiver = api.WorldManager.RegionSize / 32;
             noise = NormalizedSimplexNoise.FromDefaultOctaves(2, 0.1, 1.0, api.WorldManager.Seed + 465814);
+            lakebedLayerConfig = api.ModLoader.GetModSystem<GenPonds>().GetField<GenPonds, LakeBedLayerProperties>("lakebedLayerConfig");
+            immersionConfig = api.ModLoader.GetModSystem<ModifyLakes>().config;
         }
 
         private void OnChunkColumnGen(IServerChunk[] chunks, int chunkX, int chunkZ, ITreeAttribute chunkGenParams = null)
@@ -73,62 +79,47 @@ namespace Immersion
             {
                 for (int z = 0; z < chunksize2; z++)
                 {
-                    double n = noise.Noise(rdx + x, rdx + z);
+                    double n = noise.Noise(chunkX * chunksize + x, chunkZ * chunksize + z);
 
                     float riverRel = 1.0f - (GameMath.BiLerp(riverUpLeft, riverUpRight, riverBotLeft, riverBotRight, (float)x / chunksize2, (float)z / chunksize2) / 255f);
 
                     int y = heightMap[z * chunksize + x];
-                    
-                    //instead of breaking here, we should blend with terrain more
-                    if (y > api.World.BlockAccessor.MapSizeY / 2) break;
 
                     int dy;
                     int chunkIndex, blockIndex;
                     int rockBlockId = chunks[0].MapChunk.TopRockIdMap[z * chunksize + x];
-                    var config = api.ModLoader.GetModSystem<ModifyLakes>().config;
-                    var lakebedLayerConfig = api.ModLoader.GetModSystem<GenPonds>().GetField<GenPonds, LakeBedLayerProperties>("lakebedLayerConfig");
+                    int erosionIntensity = (y - TerraGenConfig.seaLevel) + 1 + (int)(4 * n);
+                    int lakeBed = lakebedLayerConfig.BlockCodeByMin[0].GetBlockForMotherRock(rockBlockId);
 
                     if (riverRel < 0.55 || riverRel > 0.6)
                     {
                         if (riverRel > 0.54 && riverRel < 0.61)
                         {
                             //should be a less sharp U shape
-                            for (dy = TerraGenConfig.seaLevel - 4 - (int)(4 * n); dy <= y + 1; dy++)
+                            for (dy = y - erosionIntensity; dy <= y + 1; dy++)
                             {
                                 chunkIndex = dy / chunksize;
                                 blockIndex = (chunksize * (dy % chunksize) + z) * chunksize + x;
-
-                                //shouldn't happen next to sea water when disconnected from the rest of the "river"
-                                if (chunks[chunkIndex].Blocks[blockIndex] == config.waterBlockId) continue;
+                                if (chunks[chunkIndex].Blocks[blockIndex] == config.waterBlockId && dy < TerraGenConfig.seaLevel) continue;
 
                                 chunks[chunkIndex].Blocks[blockIndex] = 
                                     dy >= TerraGenConfig.seaLevel ? 0 :
                                     dy < TerraGenConfig.seaLevel - (int)(4 * n) ? rockBlockId :
-                                    lakebedLayerConfig.BlockCodeByMin[0].GetBlockForMotherRock(rockBlockId);
+                                    lakeBed;
                             }
                         }
                         continue;
                     }
 
-                    for (dy = y + 16 + (int)(4 * n); dy > TerraGenConfig.seaLevel - 4 - (int)(4 * n); dy--)
+                    for (dy = y - erosionIntensity; dy <= y + 1; dy++)
                     {
                         if (dy >= api.World.BlockAccessor.MapSizeY) break;
                         chunkIndex = dy / chunksize;
                         blockIndex = (chunksize * (dy % chunksize) + z) * chunksize + x;
-                        if (chunks[chunkIndex].Blocks[blockIndex] == config.waterBlockId) continue;
+                        if (chunks[chunkIndex].Blocks[blockIndex] == config.waterBlockId && dy < TerraGenConfig.seaLevel) continue;
 
-                        chunks[chunkIndex].Blocks[blockIndex] = dy < TerraGenConfig.seaLevel ? config.LakeWaterBlockId : 0;
+                        chunks[chunkIndex].Blocks[blockIndex] = dy == y - erosionIntensity ? lakeBed : dy < TerraGenConfig.seaLevel ? immersionConfig.LakeWaterBlockId : 0;
                     }
-
-                    if (dy >= api.World.BlockAccessor.MapSizeY) continue;
-
-                    chunkIndex = dy / chunksize;
-                    blockIndex = (chunksize * (dy % chunksize) + z) * chunksize + x;
-
-                    if (chunks[chunkIndex].Blocks[blockIndex] == config.waterBlockId) continue;
-
-                    chunks[chunkIndex].Blocks[blockIndex] = lakebedLayerConfig.BlockCodeByMin[0].GetBlockForMotherRock(rockBlockId);
-
                 }
             }
         }
