63de909d10f9fc4caf26ee4a9695f66eb27f0084
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2020-04-28 08:42:22 -0400

    store in savegame along with global

--- Source/Systems/WorldGen/WorldgenConfig.cs
+++ Source/Systems/WorldGen/WorldgenConfig.cs
@@ -30,25 +30,83 @@ namespace Immersion
 
         public override double ExecuteOrder() => 0;
         public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Server;
+        ICoreServerAPI sapi;
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            string wPath = Path.Combine("immersion", "worldgen.json");
-
-            try
+            this.sapi = api;
+            GetWorldConfig();
+            api.Event.GameWorldSave += SaveWorldConfig;
+            sapi.RegisterCommand("immersionconfig", "Immersion Config", "", (player, group, args) =>
             {
-                ImmersionWorldgenConfig config = api.LoadModConfig<ImmersionWorldgenConfig>(wPath);
-
-                GenAquifers = genAquifers = config?.genAquifers ?? true;
-                GenRivers = genRivers = config?.genRivers ?? true;
-                GenPalms = genPalms = config?.genPalms ?? true;
-                GenDeepOreBits = genDeepOreBits = config?.genDeepOreBits ?? true;
-            }
-            catch (Exception)
-            {
-            }
-
-            api.StoreModConfig(this, wPath);
+                string word = args.PopWord();
+                bool? state = args.PopBool();
+                bool saveGlobal = args.PopBool() ?? false;
+                switch (word)
+                {
+                    case "aquifers":
+                        GenAquifers = genAquifers = state ?? !GenAquifers;
+                        player.SendMessage(0, string.Format("Worldgen parameter {0} set to {1}", word, GenAquifers), EnumChatType.OwnMessage);
+                        break;
+                    case "rivers":
+                        GenRivers = genRivers = state ?? !GenRivers;
+                        player.SendMessage(0, string.Format("Worldgen parameter {0} set to {1}", word, GenRivers), EnumChatType.OwnMessage);
+                        break;
+                    case "palms":
+                        GenPalms = genPalms = state ?? !GenPalms;
+                        player.SendMessage(0, string.Format("Worldgen parameter {0} set to {1}", word, GenPalms), EnumChatType.OwnMessage);
+                        break;
+                    case "deeporebits":
+                        GenDeepOreBits = genDeepOreBits = state ?? !GenDeepOreBits;
+                        player.SendMessage(0, string.Format("Worldgen parameter {0} set to {1}", word, GenDeepOreBits), EnumChatType.OwnMessage);
+                        break;
+                    default:
+                        break;
+                }
+                if (saveGlobal)
+                {
+                    string wPath = Path.Combine("immersion", "worldgen.json");
+                    sapi.StoreModConfig(this, wPath);
+                }
+                SaveWorldConfig();
+            }, Privilege.controlserver);
         }
+
+        public void GetWorldConfig()
+        {
+            byte[] configBytes = (sapi.WorldManager.SaveGame.GetData("ImmersionWorldGen"));
+            ImmersionWorldgenConfig storedConfig = null;
+            if (configBytes != null) storedConfig = JsonUtil.FromBytes<ImmersionWorldgenConfig>(configBytes);
+
+            if (storedConfig != null)
+            {
+                GenAquifers = genAquifers = storedConfig.genAquifers;
+                GenRivers = genRivers = storedConfig.genRivers;
+                GenPalms = genPalms = storedConfig.genPalms;
+                GenDeepOreBits = genDeepOreBits = storedConfig.genDeepOreBits;
+            }
+            else
+            {
+                string wPath = Path.Combine("immersion", "worldgen.json");
+                try
+                {
+                    ImmersionWorldgenConfig config = sapi.LoadModConfig<ImmersionWorldgenConfig>(wPath);
+
+                    GenAquifers = genAquifers = config?.genAquifers ?? true;
+                    GenRivers = genRivers = config?.genRivers ?? true;
+                    GenPalms = genPalms = config?.genPalms ?? true;
+                    GenDeepOreBits = genDeepOreBits = config?.genDeepOreBits ?? true;
+                }
+                catch (Exception)
+                {
+                }
+
+                sapi.StoreModConfig(this, wPath);
+            }
+
+            SaveWorldConfig();
+        }
+
+        public void SaveWorldConfig() => sapi.WorldManager.SaveGame.StoreData("ImmersionWorldGen", JsonUtil.ToBytes(this));
     }
 }
